43861af99348b8e342de889f50d585a168a36f07
obt <byeongtaek12@naver.com>
2025-04-21 11:50:30 +0900

    레벨 4: interceptor 적용

---
+++ src/main/java/org/example/expert/config/Interceptor.java
@@ -0,0 +1,23 @@
+package org.example.expert.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class Interceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        log.info("요청 시각: {}, URL: {}", LocalDateTime.now(), request.getRequestURL());
+
+        return true;
+    }
+
+}
